7b4daeb70181a8563c44807aa0ffa1596831d248
root <prabhatkmr5789@gmail.com>
2020-12-31 01:55:09 +0530

    add spiner to all pages

--- server/controllers/answer.js
+++ server/controllers/answer.js
@@ -3,21 +3,21 @@ const db = require('../models')
 const jwt = require('jsonwebtoken')
 
 exports.addAnswer = async (req,res,next)=>{
-    try{
+                                try{
 
-        let newAnser = await db.Answer.create({answer:req.body.answer})
-        newAnser.user = req.params.user_id
-        newAnser.question = req.params.q_id
-        let question = await db.Question.findById(req.params.q_id)
-        question.answer.push(newAnser.id)
-        await newAnser.save()
-        await question.save()
+                                    let newAnser = await db.Answer.create({answer:req.body.answer})
+                                    newAnser.user = req.params.user_id
+                                    newAnser.question = req.params.q_id
+                                    let question = await db.Question.findById(req.params.q_id)
+                                    question.answer.push(newAnser.id)
+                                    await newAnser.save()
+                                    await question.save()
 
-        res.send(newAnser)
-    }catch(err){
-        return next(err)
-    }
-}
+                                    res.send(newAnser)
+                                }catch(err){
+                                    return next(err)
+                                }
+                            }
 
 exports.updateAnswer = async (req,res,next)=>{
                             try{
@@ -40,18 +40,18 @@ exports.updateAnswer = async (req,res,next)=>{
                         }
 
 exports.deleteAnswer = async (req,res,next)=>{
-    try{
-        let token = req.headers.authorization.split(" ")[1];
-        let user = jwt.decode(token)
-                                
-       let answer = await db.Answer.findById(req.params.a_id);
-       if(answer.user==user.id){
-           answer.remove()
-           answer.save()
-           res.send('success')
-       }
-       res.send('err')
-    }catch(err){
-        return next(err)
-    }
-}
+                            try{
+                                let token = req.headers.authorization.split(" ")[1];
+                                let user = jwt.decode(token)
+                                                        
+                            let answer = await db.Answer.findById(req.params.a_id);
+                            if(answer.user==user.id){
+                                answer.remove()
+                                answer.save()
+                                res.send('success')
+                            }
+                            res.send('err')
+                            }catch(err){
+                                return next(err)
+                            }
+                        }
--- server/routes/answer.js
+++ server/routes/answer.js
@@ -7,6 +7,7 @@ const { loginRequired} = require('../middleware/user')
 router.post('/answer/:q_id/:user_id',loginRequired, addAnswer)
 
 router.put('/answer/:a_id',loginRequired,  updateAnswer)
+
 router.delete('/answer/:a_id/:user_id', loginRequired, deleteAnswer)
 
 module.exports = router;
